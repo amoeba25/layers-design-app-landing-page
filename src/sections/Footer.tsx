93e6c70ca49920ae1a3623347a0be8dce9527fa1
amoeba25 <23sohamsh@gmail.com>
2025-12-27 17:18:04 +0530

    ts error fixes

---
+++ src/sections/Footer.tsx
@@ -0,0 +1,29 @@
+import logoImage from "@/assets/images/logo.svg";
+import Image from "next/image";
+
+const footerLinks = [
+    { href: "#", label: "Contact" },
+    { href: "#", label: "Privacy Policy" },
+    { href: "#", label: "Terms & Conditions" },
+];
+
+export default function Footer() {
+    return (
+        <footer className="py-16">
+            <div className="container md:max-w-[1400px]">
+                <div className="flex flex-col md:flex-row  items-center md:justify-between gap-6">
+                    <div>
+                        <Image src={logoImage} alt="Layers logo" />
+                    </div>
+                    <div className="">
+                        <nav className="flex gap-6">
+                            {footerLinks.map((link) => (
+                                <a href={link.href} key={link.label} className="text-white/50 text-sm">{link.label}</a>
+                            ))}
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    )
+}
